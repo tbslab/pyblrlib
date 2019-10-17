63b90a9766265b73b607900ecfb89dc30c0be9a7
tbs-lab <tsubasa.i.0516@gmail.com>
2019-10-17 15:52:20 +0900

    [add] add mbgs algorithm for blrmatrix.

--- blrlib/linalg/factorization.py
+++ blrlib/linalg/factorization.py
@@ -24,16 +24,15 @@ def _tsqr_for_blrmatrix(blrmat):
     Returns
     -------
     Q : numpy.ndarray
-        A matrix with orthonormal columns. The list of matrix or lrmatrix
+        A matrix with orthonormal columns. A list of matrix and lrmatrix object.
         objects.
-    R : numpy.ndarray
+    R : matrix
         The upper triangular matrix.
     """
-    nb = blrmat.shape[0]
-    Q = numpy.full(nb, None)
+    Q = numpy.full(blrmat.shape[0], None)
     B = []
 
-    for i in range(nb):
+    for i in range(blrmat.shape[0]):
         if isinstance(blrmat.A[i, 0], lrmatrix):
             Qi, Ri = numpy.linalg.qr(blrmat.A[i, 0].U)
             Q[i] = Qi
@@ -44,7 +43,7 @@ def _tsqr_for_blrmatrix(blrmat):
     Qb, R = numpy.linalg.qr(numpy.vstack(B))
     rs, re = 0, 0
 
-    for i in range(nb):
+    for i in range(blrmat.shape[0]):
         if isinstance(blrmat.A[i, 0], lrmatrix):
             rs, re = re, re + blrmat.A[i, 0].rank
             Q[i] = lrmatrix((Q[i], Qb[rs:re, :]))
@@ -52,7 +51,7 @@ def _tsqr_for_blrmatrix(blrmat):
             rs, re = re, re + blrmat.A[i, 0].shape[0]
             Q[i] = matrix(Qb[rs:re, :])
 
-    return Q, R
+    return Q, matrix(R)
 
 
 def _mbgs_for_blrmatrix(blrmat):
@@ -70,4 +69,17 @@ def _mbgs_for_blrmatrix(blrmat):
     R : blrmatrix
         The upper triangular matrix.
     """
-    ...
+    _blrmat = blrmatrix(blrmat.A.copy())
+    Q = numpy.full(_blrmat.shape, None)
+    R = numpy.full(_blrmat.shape, None)
+    for index in numpy.ndindex(_blrmat.shape):
+        R[index] = zmatrix(_blrmat.A[index].shape)
+
+    for j in range(_blrmat.shape[1]):
+        Q[:, j], R[j, j] = _tsqr_for_blrmatrix(_blrmat[:, j])
+        for k in range(j + 1, _blrmat.shape[1]):
+            Qj = blrmatrix(Q[:, j:j + 1])
+            R[j, k] = (Qj.T @ _blrmat[:, k]).A[0, 0]
+            _blrmat.A[:, k:k + 1] -= Qj.A @ R[j:j + 1, k:k + 1]
+
+    return Q, R
