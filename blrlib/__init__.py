867e9c3ea5cd1f828a8deed163f2720fa1240237
tbs-lab <tsubasa.i.0516@gmail.com>
2019-10-17 10:46:56 +0900

    [add] add __init__.py for package.

---
+++ blrlib/__init__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+    blrlib
+    ======
+    The Package for Block Low Rank Matrix Computations.
+"""
+
+from . import core
+from . import io
+from . import linalg
+from . import vis
+
+from .core.vec import vector
+from .core.mat import zmatrix
+from .core.mat import matrix
+from .core.mat import lrmatrix
+from .core.mat import blrmatrix
+
+__all__ = []
+__all__.extend(core.__all__)
+__all__.extend(io.__all__)
+__all__.extend(linalg.__all__)
+__all__.extend(vis.__all__)
+
+__version__ = "0.0.1"
